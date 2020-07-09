344c686ee9e34743399b462956068920240aac36
Lee Rodger <lee.rodger@dvla.gov.uk>
2020-07-09 08:39:13 +0100

    Basic start - ready for dev work to begin

---
+++ src/Olivier.java
@@ -0,0 +1,10 @@
+/**
+ * Starting point for the application
+ * @author Lee Rodger
+ * @version 0.0.1
+ */
+public class Olivier {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Olivier");
+    }
+}
